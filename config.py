9b5c5b71e3b2bb6d75b1feadf3caaa4eb253de3d
TheGrimLord228 <137694592+TheGrimLord228@users.noreply.github.com>
2023-06-25 15:00:01 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -1,1 +1,7 @@
-404: Not Found
+host = 'localhost' 
+user = 'root' 
+password = '1111' 
+db_name = 'Data'
+port = 5432
+
+log_pattern = r'^(\S+) (\S+) (\S+) \[(.*?)\] "(.*?)" (\d+) (\d+) "(.*?)" "(.*?)"$'
